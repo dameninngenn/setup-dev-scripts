ff63ac826134e9cf549f11e178e8219a657c9bd9
dameninngenn <dameninngenn.owata@gmail.com>
2012-04-03 17:46:40 +0900

    v0.6.12

--- install-node.sh
+++ install-node.sh
@@ -8,9 +8,9 @@ fi
 
 git clone git://github.com/creationix/nvm.git ~/.nvm
 source ~/.nvm/nvm.sh
-nvm install v0.7.7
-nvm alias default v0.7.7
-nvm use v0.7.7
+nvm install v0.6.12
+nvm alias default v0.6.12
+nvm use v0.6.12
 
 echo "finished install nvm"
 echo "please add to .zshrc or .bashrc"
